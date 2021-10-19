ab88b92f4881ebe7a0304a43435ee8218a5d81d5
Schaffer Stewart <schaffer.stewart@lifeomic.com>
2021-10-18 22:05:37 -0400

    add print and string methods to piece

--- piece.go
+++ piece.go
@@ -1,4 +1,7 @@
 package main
+
+import "fmt"
+
 type Piece struct {
 	Tall   bool
 	Black  bool
@@ -6,7 +9,39 @@ type Piece struct {
 	Square bool
 }
 
-func (p Piece) IsIdentical(q Piece) bool {
+func (p *Piece) String() string {
+	if p == nil {
+		return "0000"
+	}
+
+	size := "T"
+	if !p.Tall {
+		size = "S"
+	}
+
+	color := "B"
+	if !p.Black {
+		color = "W"
+	}
+
+	top := "F"
+	if !p.Flat {
+		top = "D"
+	}
+
+	shape := "R" // R for rectangle
+	if !p.Square {
+		shape = "C"
+	}
+
+	return fmt.Sprintf("%s%s%s%s", size, color, top, shape)
+}
+
+func (p Piece) Print() {
+	fmt.Print(p.String())
+}
+
+func IsIdentical(p, q Piece) bool {
 	return p.Tall == q.Tall &&
 		p.Black == q.Black &&
 		p.Flat == q.Flat &&
--- piece_test.go
+++ piece_test.go
@@ -2,17 +2,43 @@ package main
 
 import "testing"
 
-func TestCreatePieceSet (t *testing.T) {
+func TestCreatePieceSet(t *testing.T) {
 	set := CreatePieceSet()
 	if (len(set)) != 16 {
 		t.Errorf("Set should contain 16 pieces. Instead got %d", len(set))
 	}
 	for i, p := range set {
 		for _, q := range set[i+1:] {
-			if p.IsIdentical(q) {
+			if IsIdentical(p, q) {
 				t.Errorf("error. duplicate pieces found p: %v, q: %v", p, q)
 			}
 		}
 	}
+}
 
+func TestPieceString(t *testing.T) {
+	table := []struct {
+		p        *Piece
+		expected string
+	}{
+		{
+			p:        &Piece{true, true, true, true},
+			expected: "TBFR",
+		},
+		{
+			p:        nil,
+			expected: "0000",
+		},
+		{
+			p:        &Piece{},
+			expected: "SWDC",
+		},
+	}
+
+	for _, tt := range table {
+		result := tt.p.String()
+		if result != tt.expected {
+			t.Errorf("piece not converted to string. Expected %s, instead got %s", tt.expected, result)
+		}
+	}
 }
