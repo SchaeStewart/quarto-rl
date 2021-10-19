87c3cb590d9d462b0b49af0d3b92fd277dc5d8ba
Schaffer Stewart <schaffer.stewart@lifeomic.com>
2021-10-18 22:17:36 -0400

    add print, string, and SetPiece methods to board

--- board.go
+++ board.go
@@ -1,15 +1,50 @@
 package main
 
-type Board struct {
-	Board [][]*Piece
-}
+import (
+	"errors"
+	"fmt"
+)
 
-func NewBoard () *Board {
-	b := [][]*Piece{
+type Board [][]*Piece
+
+func NewBoard() Board {
+	var b Board = [][]*Piece{
 		{nil, nil, nil, nil},
 		{nil, nil, nil, nil},
 		{nil, nil, nil, nil},
 		{nil, nil, nil, nil},
 	}
-	return &Board{Board: b} 
-} 
+	return b
+}
+
+func (b *Board) SetPiece(x, y int, p *Piece) error {
+	if x > 4 || y > 4 {
+		return errors.New("cannot place piece outside of board bounds")
+	}
+
+	board := *b
+
+	if board[x][y] != nil {
+		return errors.New("cannot place piece where a piece already exists")
+	}
+
+	board[x][y] = p
+
+	return nil
+}
+
+func (b Board) String() string {
+	result := ""
+	for _, x := range b {
+		result += "|"
+		for _, y := range x {
+			result += fmt.Sprintf("%s|", y)
+		}
+		result += "\n"
+	}
+	return result
+}
+
+func (b Board) PrintBoard() {
+	fmt.Print(b)
+}
--- board_test.go
+++ board_test.go
@@ -5,13 +5,40 @@ import "testing"
 func TestNewBoard(t *testing.T) {
 	b := NewBoard()
 
-	if len(b.Board) != 4 {
-		t.Errorf("expected board length to be 4. instead got %d", len(b.Board))
+	if len(b) != 4 {
+		t.Errorf("expected board length to be 4. instead got %d", len(b))
 	}
 
-	for _, x := range b.Board {
+	for _, x := range b {
 		if len(x) != 4 {
 			t.Errorf("expected row length to be 4. instead got %d", len(x))
 		}
+	}
+}
+
+func TestSetPiece(t *testing.T) {
+	b := NewBoard()
+	p := Piece{true, true, true, true}
+	b.SetPiece(0, 0, &p)
+
+	if (!IsIdentical(*b[0][0], Piece{true, true, true, true})) {
+		t.Errorf("failed to update board\n%v, Piece: %v", b, p.String())
+	}
+}
+
+func TestBoardString(t *testing.T) {
+	b := NewBoard()
+
+	emptyBoardString := "|0000|0000|0000|0000|\n|0000|0000|0000|0000|\n|0000|0000|0000|0000|\n|0000|0000|0000|0000|\n"
+
+	if (b.String() != emptyBoardString) {
+		t.Errorf("expected empty board instead got %s", b.String())
+	}
+
+	nonEmptyBoardString := "|TBFR|0000|0000|0000|\n|0000|0000|0000|0000|\n|0000|0000|0000|0000|\n|0000|0000|0000|TBFC|\n"
+	b.SetPiece(0, 0, &Piece{true, true, true, true})
+	b.SetPiece(3, 3, &Piece{true, true, true, false})
+	if (b.String() != nonEmptyBoardString) {
+		t.Errorf("expected board to be:\n%s \ninstead got:\n%s", nonEmptyBoardString, b.String())
 	}
 }
